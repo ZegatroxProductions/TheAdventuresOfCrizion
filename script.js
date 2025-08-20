97be3da0f600159efd77a1721f78ecf27c60a654
Caden G <cadendg006@gmail.com>
2025-08-20 00:05:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const characters = [
     age: '18',
     role: 'Hero',
     bday: 'September 8th',
-    bio: 'Crizion is an optimistic Reptilian who always looks on the bright side of things.'
+    bio: ' Crizion is an optimistic Reptilian who always looks on the bright side of things. He will never hesitate to help out friends or family in need. Crizion was born a few days after The Dark Skies War was declared. When his village was under siege, his father, who was an excellent warrior, goes to defend his hometown, after defending it greatly for some time, he eventually met his end as an arrow was struck through his head. As the warriors of the Shadefolk kingdom pushed closer, their home was burnt to the ground with Crizion and his mother in it, while his mother died under the collapse, Crizion lived, who was then quickly saved by an Anonymous Reptilian Warrior and brought to shelter until the war passed. From there, his grandma Pho took Crizion under her wing. Eventually, Crizion had to get a job as a blacksmith to support his grandma who was unable to work after finally retiring from her job. Crizion actually learned to like this job, there was something about sharpening weapons that he couldn’t shake. Crizion makes sure to work hard to make currency by day, and train to be a great warrior at night. He spends every night since his 13th birthday, when he was gifted with a spear, to train with it in every way possible so he can be prepared for anything, he REALLY loved to sharpen it. At age 16, Crizion was heading to work when all of a sudden he wasn’t looking and walked right in front of a royal carriage, causing Crizion to fall to the ground. The prince of the Reptilian Kingdom exited the carriage and started to cuss Crizion out. But Crizion didn’t know what to do, he didn’t know what to say, he’s never stood up to anyone like this before. The prince was fed up with his actions that Crizion didn’t mean to commit and grabbed a whip out of the carriage. Crizions eyes widened as he started to back up on all fours. The prince utter to himself before readying the whip: “This is what happens to Reptilians who don’t fall in line.” And right after, he launched the whip. In that moment, something snapped in Crizion, he gained lightning-fast instinct out of pure rage from that one sentence. He immediately uses this instinct to catch the whip around his hand and throw the royal to the ground. The royal ended up crying and said a mumble of words along the lines of revenge and left. In current day after the events, nothing was done about it and Crizion continued to work at the blacksmiths happily while spending time with his grandma. Crizions dream is to explore the world and make new friends, and to find that spark to make the world a better place!'
   },
   {
     id: 'character2',
@@ -88,6 +88,7 @@ document.querySelectorAll('.read-btn').forEach(btn => {
     window.open(`ebook-${bookId}.html`, '_blank');
   });
 });
+
 
 
 
