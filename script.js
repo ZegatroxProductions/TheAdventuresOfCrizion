4acadc66ab7e31f54cd901c058e8e39507e5b6dd
Caden G <cadendg006@gmail.com>
2025-08-20 00:10:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const characters = [
     age: '19',
     role: 'Hero',
     bday: 'October 21st',
-    bio: ' Crazytree (also known by Fern) is a clueless Zuku that never knows what’s going on.'
+    bio: 'Crazytree (also known by Fern) is a clueless Zuku that never knows what’s going on. Once he is fixated on something, he will stop paying attention to anything else, but he is also loyal to the very end when it comes to friendships. Crazytree was born about a year before the Dark Skies wars beginning, as he grew up, He was an insane kid that didn’t think too much about his choices, he did what he pleased. So instead of calling him by his real name Fern, everyone nicknamed him Crazytree. His father, who was the King of the Kingdom, had a pent-up rage from Crazytree’s actions as they got worse. Soon he couldn’t take it anymore, since lecturing had no effect, he struck Crazytree. After being struck, Crazytree was put in a state of mind where he did nothing, people thought he was paralyzed and mentally damaged and the King regretted his action the most. Crazytree continued to sit in this state of mind for years to come. It raised concern about the kings royalty position in a kingdom of peace. But in reality, during this dormant time, Crazytree was happy, because he was able to talk with his mother that passed during his birth. Crazytree didn’t know if it was real or not, but he made sure to enjoy every moment of it while he could. Even more time passed and eventually The Dark Skies war came, the Zuku\'s entire kingdom was up in flames, The King quickly grabbed Crazytree from his seated spot and ran to a secret safety chamber while Arrowite fended off Shadefolk soldiers. The chamber could only hold one person, but was immune to almost anything, it was reserved for the king. But instead, The King shoved Crazytree in there along with the powerful gem from his staff, the chamber was sealed off and the king went up in flames. After a long slumber that caused him to become mute in the process, he was released by no other than Crizion himself, to which he gave his utmost loyalty to Crizion. Crazytree try’s his best to tend to peaceful tactics, but knows that violence must sometimes be an option with the great power he has. His dream is to restore peace to the world with his buddy Crizion.'
   },
 ];
 
@@ -88,6 +88,7 @@ document.querySelectorAll('.read-btn').forEach(btn => {
     window.open(`ebook-${bookId}.html`, '_blank');
   });
 });
+
 
 
 
